31a36e560da89e83b8606362771ef62b94ad1460
LocTran016 <locphuc016@gmail.com>
2021-08-24 12:15:42 +0700

    feat: default value for card templates and seperated data.json

---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const allSubjects = {
+  math: {
+    fullName: 'Toán học',
+    // teacher: 'Lorem ipsum dolor sit amet.',
+    bgColor: 'from-red-400 to-yellow-400 dark:from-red-500 dark:to-yellow-500',
+    mainImg:
+      'https://sachcuatui.net/wp-content/uploads/2019/10/sach-giao-khoa-dai-so-lop-10-nang-cao.jpg',
+    first: [],
+    second: [],
+    third: [],
+    calcAverage() {
+      let division = 1;
+      let average;
+      let total = 0;
+      division +=
+        this.first.length + this.second.length * 2 + this.third.length * 3;
+      if (division > 1) {
+        for (let i = 0; i < this.first.length; i++) {
+          total += this.first.length;
+        }
+        for (let i = 0; i < this.second.length; i++) {
+          total += this.second.length;
+        }
+        for (let i = 0; i < this.third.length; i++) {
+          total += this.third.length;
+        }
+        average = (total / division).toFixed(2);
+      } else {
+        average = '__.__';
+      }
+      this.average = average;
+      return average;
+    },
+  },
+};
